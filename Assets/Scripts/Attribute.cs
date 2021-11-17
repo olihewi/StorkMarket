9f2929a85cce88addf2d7b40ab55956828b8be7b
Oliver <hewisonoliver@gmail.com>
2021-11-17 11:24:10 +0000

    Attribute example

---
+++ Assets/Scripts/Attribute.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName="New Attribute", menuName="Stork Market/Attribute")]
+public class Attribute : ScriptableObject
+{
+    public string description;
+}
