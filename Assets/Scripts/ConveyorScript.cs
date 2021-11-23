2787fe807f224108ee7eaef1d034acc89049d403
Andrei <andrei2.barlea@live.uwe.ac.uk>
2021-11-23 12:11:36 +0000

    conveyor belt

---
+++ Assets/Scripts/ConveyorScript.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorScript : MonoBehaviour
+{
+    public float speed;
+
+    public Vector2 direction;
+
+    public List<GameObject> onBelt;
+    
+   
+    
+
+    // Update is called once per frame
+  /*  void Update()
+    {
+        for (int i = 0; i <= onBelt.Count - 1; i++)
+        {
+            onBelt[i].GetComponent<Rigidbody2D>().velocity = speed * direction * Time.deltaTime;
+        }
+    } */
+    
+    private void OnCollisionTrigger(Collision collision)
+    {
+        onBelt.Add(collision.gameObject);
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        onBelt.Remove(collision.gameObject);
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        collision.gameObject.GetComponent<Rigidbody2D>().velocity = speed * direction * Time.deltaTime;
+    }
+}
