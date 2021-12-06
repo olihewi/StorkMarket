5b83182a1117f8bb53d002816d6864c6e4b1ae8d
Oliver <hewisonoliver@gmail.com>
2021-12-06 13:13:02 +0000

    Basic Request System

--- Assets/Scripts/Request.cs
+++ Assets/Scripts/Request.cs
@@ -6,16 +6,36 @@ using TMPro;
 [RequireComponent(typeof(TextMeshPro))]
 public class Request : MonoBehaviour
 {
-    private TextMeshPro text;
+    [System.Serializable]
+    public struct RequestFormat
+    {
+        public string prefix;
+        public string type;
+        public string attribute;
+        public string suffix;
+    }
+    private TextMeshPro textMesh;
     private static Attribute[] ATTRIBUTES;
     private static Type[] TYPES;
+    public RequestFormat[] requestFormats = new RequestFormat[0];
     // Start is called before the first frame update
     void Start()
     {
-        text = GetComponent<TextMeshPro>();
+        textMesh = GetComponent<TextMeshPro>();
         ATTRIBUTES = Resources.LoadAll<Attribute>("Descriptors");
         TYPES = Resources.LoadAll<Type>("Descriptors");
-        text.text = TYPES[Random.Range(0, TYPES.Length)].name;
+        RequestFormat requestFormat = requestFormats[Random.Range(0, requestFormats.Length)];
+        Type thisType = TYPES[Random.Range(0, TYPES.Length)];
+
+        string text = requestFormat.prefix.Replace("\\n", "\n");
+        text += requestFormat.type.Replace("\\type", thisType.name).Replace("\\n", "\n");
+        for (int i = 0; i < Random.Range(0,4); i++)
+        {
+            Attribute thisAttribute = ATTRIBUTES[Random.Range(0, ATTRIBUTES.Length)];
+            text += requestFormat.attribute.Replace("\\attribute", thisAttribute.name).Replace("\\n","\n");
+        }
+        text += requestFormat.suffix.Replace("\\n", "\n");
+        textMesh.text = text;
     }
 
     // Update is called once per frame
